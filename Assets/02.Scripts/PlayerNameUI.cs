deb561bf92448ad67273ab9c912be9576641216a
paul991006 <1099paul@gmail.com>
2025-08-29 18:45:44 +0900

    08.29
    다른 플레이어 닉네임 보이게 수정 완료

--- Assets/02.Scripts/PlayerNameUI.cs
+++ Assets/02.Scripts/PlayerNameUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using Unity.Collections;
 using Unity.Netcode;
@@ -8,43 +9,43 @@ public class PlayerNameUI : NetworkBehaviour
     [SerializeField] private TMP_Text nameText;
     private Transform cameraTransform;
 
-    private NetworkVariable<FixedString32Bytes> playerName = new NetworkVariable<FixedString32Bytes>();
+    private NetworkVariable<FixedString32Bytes> playerName = new NetworkVariable<FixedString32Bytes>("", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
     public override void OnNetworkSpawn()
     {
-        // 오너 플레이어라면 서버에 닉네임 등록
-        if (IsOwner)
-        {
-            string nickname = AuthManager.Instance.CurrentNickname; // 본인 닉네임 가져오기
-            SetPlayerNameServerRpc(nickname);
-
-            // 자신의 카메라 찾아서 UI가 바라보게 하기
-            Camera myCam = GetComponentInChildren<Camera>();
-            if (myCam != null)
-                cameraTransform = myCam.transform;
-        }
-        else
-        {
-            // 다른 플레이어는 로컬 오너의 카메라를 바라보게 하기
-            cameraTransform = Camera.main != null ? Camera.main.transform : null;
-        }
-
+        Debug.Log($"[PlayerNameUI] My Nickname at Spawn: {AuthManager.Instance.CurrentNickname}");
         // 닉네임 동기화 이벤트 연결
         playerName.OnValueChanged += OnNameChanged;
         nameText.text = playerName.Value.ToString();
+
+        // 오너 플레이어라면 서버에 닉네임 등록
+        if (IsOwner)
+        {
+            StartCoroutine(SetNameNextFrame());
+
+        }
     }
 
-    private void OnDestroy()
+    private IEnumerator SetNameNextFrame()
+    {
+        yield return null;
+        string nickname = AuthManager.Instance.CurrentNickname; // 본인 닉네임 가져오기
+        if (!string.IsNullOrEmpty(nickname))
+        {
+            SetPlayerNameServerRpc(nickname);
+        }
+    }
+
+    public override void OnNetworkDespawn()
     {
         playerName.OnValueChanged -= OnNameChanged;
     }
 
     private void Update()
     {
-        if (cameraTransform != null)
+        if (Camera.main != null)
         {
-            transform.LookAt(cameraTransform);
-            transform.Rotate(0, 180f, 0);
+            nameText.transform.rotation = Quaternion.LookRotation(nameText.transform.position - Camera.main.transform.position);
         }
     }
 
@@ -53,7 +54,7 @@ public class PlayerNameUI : NetworkBehaviour
         nameText.text = newValue.ToString();
     }
 
-    [ServerRpc]
+    [ServerRpc(RequireOwnership = false)]
     private void SetPlayerNameServerRpc(string newName)
     {
         playerName.Value = newName;
